755bfedc1429e229be849c2da1b79752e18fcbb3
alexforsale <alexforsale@yahoo.com>
2024-02-22 14:53:22 +0700

    org-roam.db location part 2

--- scripts/build.el
+++ scripts/build.el
@@ -10,8 +10,11 @@
 (require 'org-roam)
 (require 'org-roam-ui)
 (setq org-roam-directory default-directory)
-(setq org-roam-db-location (expand-file-name "org/org-roam.db" default-directory))
+(setq org-roam-db-location (expand-file-name "orgs/org-roam.db" default-directory))
 (org-roam-db-sync)
+
+(unless (file-directory-p (expand-file-name "orgs" default-directory))
+  (make-directory (expand-file-name "orgs" default-directory)))
 
 (defun collect-backlinks-string (backend)
   (when (org-roam-node-at-point)
